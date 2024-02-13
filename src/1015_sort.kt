7f5be2993dd626edc22e408efed0af8dd3a4275e
seon <ensony713@gmail.com>
2024-02-13 22:03:57 +0900

    remote

--- src/1015_sort.kt
+++ src/1015_sort.kt
@@ -6,13 +6,15 @@ fun main () {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val n = br.readLine().toInt()
     val arr = Array(n) { Pair(0, 0) }
+    val origin = Array(n) { 0 }
     br.readLine().split(" ").forEachIndexed{ index, item ->
-        arr[index] = Pair(item.toInt(), index)
+        arr[index] = Pair(index, item.toInt())
+        origin[index] = item.toInt()
     }
 
-    arr.sortWith(compareByDescending { it.first })
+    arr.sortWith(compareBy { it.second })
 
-    arr.asList().asReversed().forEach{
-        print("${it.second} ")
+    origin.forEachIndexed { index, it ->
+        print("${arr[it]} ")
     }
 }
