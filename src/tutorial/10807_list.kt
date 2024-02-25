3944c87a9b5da760380a0dfb9d561b002b81988d
seon <ensony713@gmail.com>
2024-02-25 20:52:14 +0900

    update

--- src/tutorial/10807_list.kt
+++ src/tutorial/10807_list.kt
@@ -13,9 +13,9 @@ fun main() {
     val line = bsr.readLine()
     val arr = Array<Int>(205) { 0 }
     val arr2 = List(10) { i -> i }
-
     // val arr = Array(205) { 0 } // 이런 형식도 가능
     // 1, 2, 3, 4, 5 를 넣으려고 하면 lambda로 { i -> i }를 주면 됨
+
     val input = line.split(" ")
 
     for (i in input) {
